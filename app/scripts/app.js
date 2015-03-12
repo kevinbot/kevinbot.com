502c9dfea7ec2fd0cf9b4afb6585be3f19792953
Ryan Shea <ryan@shea.io>
2015-03-12 11:01:01 -0400

    update routing to html5Mode (slashes over hashes)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,17 +13,15 @@ angular
     'ngResource',
     'ngRoute'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
       .otherwise({
         redirectTo: '/'
       });
+
+    $locationProvider.html5Mode(true).hashPrefix('!');
   });
