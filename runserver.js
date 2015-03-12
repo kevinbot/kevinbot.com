438dc17a71ce9a08590cb46087377c1f048eef56
Ryan Shea <ryan@shea.io>
2015-03-12 17:53:15 -0400

    remove image file renaming

--- runserver.js
+++ runserver.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.all('/*', function(req, res) {
-    res.sendfile(__dirname + '/dist/index.html');
+    res.sendFile(__dirname + '/dist/index.html');
 });
 
 app.listen(port);
